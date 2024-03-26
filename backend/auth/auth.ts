3078a3758f084b2ec959f4b7573efba1fa32e348
CocoTapi <shioriavino@gmail.com>
2024-03-25 23:35:37 -0400

    update code

--- backend/auth/auth.ts
+++ backend/auth/auth.ts
@@ -1,9 +1,27 @@
 import Database from "../database/Database";
 import bcrypt from "bcrypt";
-import { LoginInfo, SignUpInfo, UserInfo } from "../util/types";
+import { LoginInfo, SignUpInfo, UserInfo, GoogleUser } from "../util/types";
 import env from 'dotenv';
+import { OAuth2Client } from "google-auth-library";
 
 env.config();
+
+const redirectUrl = 'http://localhost:8080/auth/google';
+
+const oAuth2Client = new OAuth2Client(
+  process.env.GOOGLE_CLIENT_ID,
+  process.env.GOOGLE_CLIENT_SECRET,
+  redirectUrl
+);
+
+const domainRedirect = {
+  scope: [
+    'https://www.googleapis.com/auth/userinfo.email',
+    'https://www.googleapis.com/auth/userinfo.profile',
+  ],
+  access_type: 'offline',
+  prompt: 'consent',
+}
 
 const db = Database.db;
 const saltRounds = parseInt(process.env.SALTROUNDS as string);
@@ -52,10 +70,60 @@ export async function login({ email, password }: LoginInfo) {
 
 export async function getUserDataFromGoogle(access_token: any): Promise<any> {
   console.log("access token: ", access_token)
-  const response = await fetch(
-    `https://www.googleapis.com/oauth2/v3/userinfo?access_token=${access_token}`
-  )
+  const ticket = await oAuth2Client.verifyIdToken({
+    idToken: access_token,
+    audience: process.env.GOOGLE_CLIENT_ID,
+  });
 
-  const data: any = await response.json();
-  return data;
+  const payload = ticket.getPayload();
+    if(!payload) throw new Error('Error getting payload');
+
+    // Here, payload contains user information
+    const userId = payload['sub'];
+    const userEmail = payload['email'];
+    const userName = payload['name'];
+
+    console.log(userId);
+    console.log(userEmail);
+    console.log(userName);
+  // const response = await fetch(
+  //   `https://www.googleapis.com/oauth2/v3/userinfo?access_token=${access_token}`
+  // )
+
+  // const data: GoogleUser = await response.json() as GoogleUser;
+  // console.log("data: ", data);
+  return {userId, userEmail, userName};
 }
+
+export async function oAuthLogin(){
+  const authUrl = oAuth2Client.generateAuthUrl(domainRedirect);
+  if(!authUrl) throw new Error ("Auth URL could not be generated");
+  console.log(authUrl)
+  return authUrl;
+ 
+}
+// export async function oAuthToDB(userData){
+//   const { email,  given_name, family_name }: GoogleUser = userData;
+//   const date = new Date();
+
+//   const checkResult = await db.query("SELECT * FROM users WHERE email = $1", [
+//     email,
+//   ]);
+  
+  
+//   if (checkResult.rows.length > 0) {
+//     const user = checkResult.rows[0];
+//     db.query("UPDATE users SET last_login = $1 WHERE user_id = $2", [
+//      date, user.user_id
+//     ]);
+//     console.log("Updated login date.")
+//   } else {
+//     await db.query(
+//       "INSERT INTO users (email, password, first_name, last_name, created_date, last_update) VALUES ($1, $2, $3, $4, $5, $6) RETURNING *",
+//       [email, "google", given_name, family_name, date, date]
+//     );
+  
+//     console.log("registered User:", email,  given_name, family_name, date, date);
+//   }
+
+//}
--- backend/auth/authRoutes.ts
+++ backend/auth/authRoutes.ts
@@ -1,8 +1,7 @@
 import express from "express";
-import { signUp, login, getUserDataFromGoogle } from "./auth";
+import { signUp, login, getUserDataFromGoogle, oAuthLogin } from "./auth";
 import { asyncHandler } from "../util/route-util";
 import { LoginInfo, SignUpInfo } from "../util/types";
-import { OAuth2Client } from 'google-auth-library';
 import env from "dotenv";
 
 env.config();
@@ -23,40 +22,37 @@ router.post("/login", asyncHandler(async (req, res) => {
 }));
 
 //cliant info
-const redirectUrl = 'http://localhost:8080/auth/google';
 
-const oAuth2Client = new OAuth2Client(
-    process.env.GOOGLE_CLIENT_ID,
-    process.env.GOOGLE_CLIENT_SECRET,
-    redirectUrl
-);
+
 
 //create the url for google login and send it back to the frontend
 router.post("/oauth", asyncHandler(async (req, res) => {
-    res.header('Access-Control-Allow-Origin', 'http://localhost:3000/login');
-    res.header("Referrer-Policy", 'no-referrer-when-downgrade');
+    // res.header('Access-Control-Allow-Origin', 'http://localhost:3000/auth');
+    // res.header("Referrer-Policy", 'no-referrer-when-downgrade');
 
-    const authorizeUrl = oAuth2Client.generateAuthUrl({
-        access_type: 'offline',
-        scope: 'https://www.googleapis.com/auth/userinfo.profile',
-        prompt: 'consent'
-    });
-
-    res.json({ url: authorizeUrl });
+    const redirectUrl = await oAuthLogin()
+    res.status(200).json({ redirectUrl })
+    // res.json({ url: authorizeUrl });
 }));
 
-router.get("/oauth", asyncHandler(async function (req, res) {
-    const code: string = req.query.code as string;
-    if (!code) res.status(400).json({ message: 'Authorization code is missing' });
+router.get("/auth/google", asyncHandler(async function (req, res) {
+    // const code: string = req.query.code as string;
+    // if (!code) res.status(400).json({ message: 'Authorization code is missing' });
 
-    const token = await oAuth2Client.getToken(code);
-    await oAuth2Client.setCredentials(token.tokens);
-    const user = oAuth2Client.credentials;
-    const userData = await getUserDataFromGoogle(user.access_token);
-    res.status(200).json({
-        message: "Success",
-        data: userData
-    })
+    // const token = await oAuth2Client.getToken(code);
+
+    // //allows the client to use the access token
+    // // await oAuth2Client.setCredentials(token.tokens);
+
+    // const user = oAuth2Client.credentials;
+    // const userData = await getUserDataFromGoogle(user.access_token);
+
+    // //await oAuthToDB(userData);
+
+    // res.status(200).json({
+    //     message: "Success",
+    //     data: userData.given_name
+    // })
 }))
 
 export default router;
