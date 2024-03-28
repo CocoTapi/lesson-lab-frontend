78c005fe0a18ba57ce16547a34083b39d7c5c0eb
CocoTapi <shioriavino@gmail.com>
2024-03-28 01:36:40 -0400

    delete code

--- my-best/src/components/OAuthRedirectHandler.js
+++ my-best/src/components/OAuthRedirectHandler.js
@@ -1,21 +1,19 @@
 //import React, { useEffect } from 'react';
 //import { useLocation, useHistory } from 'react-router-dom';
-import { handleGoogleAuthEvent } from '../pages/util/checkAuth';
 
 function OAuthRedirectHandler(){
-    handleGoogleAuthEvent()
     // access information about the current URL
     //const location = useLocation();
     // navigate to different routes
-    // const history = useHistory();
+    //const history = useHistory();
 
     // useEffect(() => {
     //     // Check if the current location contains authentication data from Google OAuth
     //     // For example, if Google redirects back to your application with a token or code
     //     // You would extract that data from the location object and perform authentication checks
 
-    //     // Example: Check if location contains a token
-    //     const token = new URLSearchParams(location.search).get('token');
+    //     // get token here;
+
     //     if (token) {
     //         // Perform authentication checks (e.g., validate token with your backend)
     //         // If authentication is successful, redirect to the home page
