0d287606e0a2cf98aac106428aa1881049ac937e
CocoTapi <shioriavino@gmail.com>
2024-05-02 12:45:02 -0400

    add activity nav styling

--- my-best/src/components/activities/ActivityNavigation.js
+++ my-best/src/components/activities/ActivityNavigation.js
@@ -1,31 +1,50 @@
 import { NavLink, useRouteLoaderData } from "react-router-dom";
+import { useState } from "react";
+import classes from '../css/activities/ActivityNavigation.module.css';
+import { TiPlus } from "react-icons/ti";
+import { FiSearch } from "react-icons/fi";
 
 function ActivityNavigation(){
+    const [searchTerm, setSearchTerm] = useState('');
     const user = useRouteLoaderData('root');
     let token;
     if(user) token = user.token;
 
+    const handleSearchTermChange = (event) => {
+        setSearchTerm(event.target.value);
+    }
+
+    //TODO: handle search term
+
+
+
     return (
-        <header>
-            <nav>
-                <ul>
-                    <li>
-                        <NavLink
-                            to="/activities"
-                        >
-                            All Activities
-                        </NavLink>
-                    </li>
-                    <li>
-                        <NavLink
-                            to={token ? "/activities/new" : "../auth?mode=login"}
-                        >
-                            Add Activity
-                        </NavLink>
-                    </li>
-                </ul>
-            </nav>
-        </header>
+        <div className={classes.frame}>
+            <ul className={classes.left}>
+                <li className={classes.title}>
+                    <NavLink
+                        to="/activities"
+                    >
+                        Activities
+                    </NavLink>
+                </li>
+                <li>
+                    <NavLink
+                        to={token ? "/activities/new" : "../auth?mode=login"}
+                    >
+                        <button className={classes.addButton}>
+                            <TiPlus />
+                            <span>Add Activity</span> 
+                        </button>
+                    </NavLink>
+                </li>
+            </ul>
+            <div>
+                <form>
+                    <input className={classes.inputFrame} value={searchTerm} onChange={handleSearchTermChange} placeholder="Search"/>
+                </form>
+            </div>
+        </div>
     )
 }
 
