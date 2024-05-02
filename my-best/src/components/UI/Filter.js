7b1fb5af8616149a26a38c32867d7ee90466953e
CocoTapi <shioriavino@gmail.com>
2024-05-02 11:26:59 -0400

    update filter style

--- my-best/src/components/UI/Filter.js
+++ my-best/src/components/UI/Filter.js
@@ -2,7 +2,7 @@ import classes from '../css/UI/Filter.module.css';
 import ButtonS from './ButtonS';
 
 
-function Filter({ onTimeChange, onAgeChange }) {
+function Filter({ onTimeChange, onAgeChange, onTagChange }) {
  
 
     const handleTimeChange = (event) => {
@@ -13,31 +13,46 @@ function Filter({ onTimeChange, onAgeChange }) {
         onAgeChange(event.target.value);
     };
 
+    const handleTagChange = (event) => {
+        onTagChange(event.target.value);
+    }
+
     return (
         <div className={classes.box}>
             <form>
-                <h1>Filter</h1>
+                <div className={classes.contents}>
+                    <h1>Filter</h1>
 
-                <h2>Duration</h2>
-                <div className={classes.radio}>
-                    <label><input type="radio" value="~5 mins" onChange={handleTimeChange} /> ~ 5 mins</label>
-                    <label><input type="radio" value="10 mins" onChange={handleTimeChange} /> 10 mins</label>
-                    <label><input type="radio" value="15 mins" onChange={handleTimeChange} /> 15 mins</label>
-                    <label><input type="radio" value="20 mins" onChange={handleTimeChange} /> 20 mins</label>
-                    <label><input type="radio" value="25 mins" onChange={handleTimeChange} /> 25 mins</label>
-                    <label><input type="radio" value="30 ~ mins" onChange={handleTimeChange} /> 30 ~ mins</label>   
-                </div>    
+                    <h2>Duration</h2>
+                    <div className={classes.radio}>
+                        <label><input type="radio" value="~5 mins" onChange={handleTimeChange} /> ~ 5 mins</label>
+                        <label><input type="radio" value="10 mins" onChange={handleTimeChange} /> 10 mins</label>
+                        <label><input type="radio" value="15 mins" onChange={handleTimeChange} /> 15 mins</label>
+                        <label><input type="radio" value="20 mins" onChange={handleTimeChange} /> 20 mins</label>
+                        <label><input type="radio" value="25 mins" onChange={handleTimeChange} /> 25 mins</label>
+                        <label><input type="radio" value="30 ~ mins" onChange={handleTimeChange} /> 30 ~ mins</label>   
+                    </div>    
 
-                <h2>Age Group</h2>
-                <div className={classes.radio}>
-                    <label><input type="radio" value="kids" onChange={handleAgeChange}/> For kids</label>
-                    <label><input type="radio" value="teens" onChange={handleAgeChange}/> For teens</label>
-                    <label><input type="radio" value="adults" onChange={handleAgeChange}/> For adults</label>
-                    <label><input type="radio"value="all age" onChange={handleAgeChange}/> For all ages</label>
-                </div>
-                <div className={classes.view}>
-                    <button className={classes.clear}>clear all</button>
-                    <ButtonS>view</ButtonS>
+                    <h2>Age Group</h2>
+                    <div className={classes.radio}>
+                        <label><input type="radio" value="kids" onChange={handleAgeChange}/> for kids</label>
+                        <label><input type="radio" value="teens" onChange={handleAgeChange}/> for teens</label>
+                        <label><input type="radio" value="adults" onChange={handleAgeChange}/> for adults</label>
+                        <label><input type="radio"value="all age" onChange={handleAgeChange}/> for all ages</label>
+                    </div>
+
+                    <h2>Popular Categories</h2>
+                    <div className={classes.radio}>
+                        <label><input type="radio" value="icebreakers" onChange={handleTagChange}/> icebreakers</label>
+                        <label><input type="radio" value="fun" onChange={handleTagChange}/> fun</label>
+                        <label><input type="radio" value="interactive" onChange={handleTagChange}/> interactive</label>
+                        <label><input type="radio"value="minimum prep" onChange={handleTagChange}/> minimum prep</label>
+                    </div>
+
+                    <div className={classes.viewButtons}>
+                        <button className={classes.clearButton}>clear all</button>
+                        <ButtonS>view</ButtonS>
+                    </div>
                 </div>
             </form>
         </div>
