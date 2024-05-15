e8d52b9aadc67d505675014a52cbc7abd5c85319
CocoTapi <shioriavino@gmail.com>
2024-05-15 00:33:54 -0400

    update activity list styling

--- my-best/src/components/UI/SortBar.js
+++ my-best/src/components/UI/SortBar.js
@@ -1,6 +1,8 @@
 import classes from '../css/UI/SortBar.module.css'
+import { useState, useEffect } from 'react';
 import { colorSchemes } from './colorSchemes';
 import ButtonM from './ButtonM';
+import { FaSortAmountDown } from "react-icons/fa";
 
 function SortBar (
     { 
@@ -15,6 +17,31 @@ function SortBar (
     buttonColor
 }) {
     const { textColor, backgroundColor, borderColor } = colorSchemes[colorScheme] || colorSchemes.primary;
+    const [showMenuBar, setShoeMenuBar] = useState(false);
+    const [displayMenu, setDisplayMenu] = useState(false);
+ 
+    useEffect(() => {
+        const handleResize = () => {
+            if (window.innerWidth < 640) {
+                setShoeMenuBar(true);
+            } else {
+                setShoeMenuBar(false);
+            }
+        };
+
+        handleResize();
+
+        // Listen for resize events
+        window.addEventListener('resize', handleResize);
+
+        // Clean up
+        return () => window.removeEventListener('resize', handleResize);
+    }, []);
+
+    const handleOpenMenu = () => {
+        setDisplayMenu(!displayMenu); 
+    }
+
     
     const barStyle = {
         color: textColor,
@@ -38,27 +65,46 @@ function SortBar (
             className={classes.bar}
             style={barStyle}
         >
-            <h1>Sort by</h1>
-            <form>
-                <select id='sort' type='sort' name='sort' onChange={sortChangeHandler} className={classes.formSelect}>
-                    <option value="shortToLong">Duration: Short to Long</option>
-                    <option value="longToShort">Duration: Long to Short</option>
-                    <option value="TopRated">User's Top Rated</option>
-                    <option value="New">New Arrivals</option>
-                </select> 
-            </form>
-            {search === 'true' && 
-                <form className={classes.right}>
-                    <input className={classes.inputFrame} onChange={handleSearchTermChange} placeholder="Search"/>
+           {!showMenuBar && 
+           <>
+                <h1>Sort by</h1>
+                <form>
+                    <select id='sort' type='sort' name='sort' onChange={sortChangeHandler} className={classes.formSelect}>
+                        <option value="shortToLong">Duration: Short to Long</option>
+                        <option value="longToShort">Duration: Long to Short</option>
+                        <option value="TopRated">User's Top Rated</option>
+                        <option value="New">New Arrivals</option>
+                    </select> 
                 </form>
+                {search === 'true' && 
+                    <form className={classes.right}>
+                        <input className={classes.inputFrame} onChange={handleSearchTermChange} placeholder="Search"/>
+                    </form>
+                }
+                {button === 'ButtonM' &&
+                <div className={classes.right}>
+                        <ButtonM colorScheme={buttonColor} onClick={onClick} >
+                        <h2 className={classes.buttonIcon}>{icon}</h2>
+                        <p>{buttonWord}</p>
+                    </ButtonM>
+                </div>
+                }
+            </>
             }
-            {button === 'ButtonM' &&
-            <div className={classes.right}>
-                 <ButtonM colorScheme={buttonColor} onClick={onClick} >
-                    <h2 className={classes.buttonIcon}>{icon}</h2>
-                    <p>{buttonWord}</p>
-                </ButtonM>
-            </div>
+            {showMenuBar && 
+                <>
+                    <div onClick={handleOpenMenu} className={classes.sortIcon}><FaSortAmountDown /></div>
+                </>
+            }
+            {displayMenu && 
+                <form>
+                    <select id='sort' type='sort' name='sort' onChange={sortChangeHandler} className={classes.formSelect}>
+                        <option value="shortToLong">Duration: Short to Long</option>
+                        <option value="longToShort">Duration: Long to Short</option>
+                        <option value="TopRated">User's Top Rated</option>
+                        <option value="New">New Arrivals</option>
+                    </select> 
+                </form>
             }
         </div>
     )
