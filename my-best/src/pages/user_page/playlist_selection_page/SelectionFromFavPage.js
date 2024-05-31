038c4e9a238445aceb9d1ded482c1ead0ba34501
CocoTapi <shioriavino@gmail.com>
2024-05-30 22:30:28 -0400

    setup patch request to update playlist activities

--- my-best/src/pages/user_page/playlist_selection_page/SelectionFromFavPage.js
+++ my-best/src/pages/user_page/playlist_selection_page/SelectionFromFavPage.js
@@ -1,8 +1,9 @@
 import { Suspense } from "react";
-import { defer, useRouteLoaderData, Await } from "react-router-dom";
+import { defer, useRouteLoaderData, Await, json, redirect } from "react-router-dom";
 import { getAuthToken } from "../../util/checkAuth";
 import { loadUserFavorites } from "../UserFavoritesPage";
 import SelectionForm from "../../../components/user_page/playlist_selection/SelectionForm";
+import { API_URL } from "../../../App";
 
 function SelectionFromFavPage(){
     const { data, playlist_id } = useRouteLoaderData('playlist-selecction-favorites');
@@ -38,12 +39,37 @@ export async function loader({ request, params }){
 
 export async function addActivityIntoPlaylistAction({ request }){
     const method = request.method;
+    const token = getAuthToken();
     const formData = await request.formData();
-    const activity_id_list = formData.get("activity_id_list");
     const user_id = formData.get("user_id");
     const playlist_id = formData.get("playlist_id");
-    const token = getAuthToken();
+    const list = formData.get("activity_id_list");
+    const activity_id_arr = list.split(',').map(Number);
+
+    const playlistData = {
+        user_id: parseInt(user_id),
+        playlist_id: parseInt(playlist_id),
+        activity_id_list: activity_id_arr
+    }
+
+    const response = await fetch(`${API_URL}/${user_id}/playlists`, {
+        method: method,
+        headers: {
+            'Content-Type' : 'application/json',
+            'Authorization': `Bearer ${token}`
+        },
+        body: JSON.stringify(playlistData)
+    })
+    
+
+    if (response.status === 422 || response.status === 401) {
+        return response;
+    };
+
+    if (!response.ok){
+        throw json({ message: 'Could not save activity.'}, { status: 500 });
+    }
 
 
-    console.log(activity_id_list);
+    return redirect(`/mypage/${user_id}/playlists`);
 }
